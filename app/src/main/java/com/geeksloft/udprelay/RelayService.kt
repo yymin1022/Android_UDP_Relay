9ad9ad881fc7aef8aa58dd3ae782870c6cba99b6
yymin1022 <yymin1022@gmail.com>
2024-07-16 10:47:01 +0900

    Update RelayService : Added UDP Listener

--- app/src/main/java/com/geeksloft/udprelay/RelayService.kt
+++ app/src/main/java/com/geeksloft/udprelay/RelayService.kt
@@ -4,21 +4,70 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.util.Log
+import java.net.DatagramPacket
+import java.net.DatagramSocket
 
 class RelayService : Service() {
     private val LOG_TAG = "Relay Service"
+    private var serverSocket: DatagramSocket? = null
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
-        Log.i(LOG_TAG, "Service Started")
+        Log.d(LOG_TAG, "Starting Service")
+
+        initSocket()
+        SocketThread().start()
+
+        Log.d(LOG_TAG, "Started Service")
         return START_STICKY
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i(LOG_TAG, "Service Stopped")
+        Log.d(LOG_TAG, "Stopping Service")
+
+        SocketThread().join()
+        closeSocket()
+
+        Log.d(LOG_TAG, "Stopped Service")
     }
 
     override fun onBind(intent: Intent): IBinder? {
         return null
+    }
+
+    private fun initSocket() {
+        if(serverSocket == null){
+            serverSocket = DatagramSocket(8900)
+        }
+    }
+
+    private fun closeSocket() {
+        if(serverSocket != null){
+            serverSocket!!.close()
+            serverSocket = null
+        }
+    }
+
+    inner class SocketThread: Thread() {
+        override fun run() {
+            super.run()
+
+            if(serverSocket == null) {
+                Log.e(LOG_TAG, "Socket not initialized!!")
+                return
+            }
+
+            Log.d(LOG_TAG, "Server Listening on ${serverSocket!!.localAddress.hostAddress}:${serverSocket!!.localPort}")
+            try {
+                val receiveBuffer = ByteArray(1024)
+                val receivePacket = DatagramPacket(receiveBuffer, receiveBuffer.size)
+                while(true) {
+                    serverSocket!!.receive(receivePacket)
+                    Log.i(LOG_TAG, "Received ${receivePacket.data.contentToString()}")
+                }
+            } catch(e: Exception) {
+                Log.e(LOG_TAG, e.toString())
+            }
+        }
     }
 }
